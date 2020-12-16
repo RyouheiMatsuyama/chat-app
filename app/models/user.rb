0c9ef90e17de91b6c1dcb1dcb2452e253ab67a40
RyouheiMatsuyama <ryohei.0427@ezweb.ne.jp>
2020-12-16 16:32:19 +0900

    空のメッセージが送信できないバリデーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,10 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  
   has_many :room_users
   has_many :rooms, throught: :room_users
   has_many :messages
-  
   validates :name, presence: true
 end
